2aa1b0d517fbc84dcfdd8c41005a85c3e3e8a3cd
gustavo.bossolani <gustavo.bossolani@resource.com.br>
2020-01-06 16:53:31 -0300

    sequelize configurado

---
+++ src/database/seeds/20200106190739-admin-user.js
@@ -0,0 +1,16 @@
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    },
+
+    down: () => {},
+};
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, resp) => resp.json({ message: 'hello world' }));
+routes.get('/', async (req, resp) => {
+    const user = await User.create({
+        name: 'Gustavo Bossolani',
+        email: 'gustavo.email.com',
+        password_hash: '123123123',
+    });
+
+    resp.json(user);
+});
 
 export default routes;
