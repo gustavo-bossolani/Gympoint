7365bf1e81170718b8c2600eae0df5a3247fd964
gustavo-bossolani <gustavo.wyd@outlook.com>
2020-01-30 15:28:12 -0300

    lista checkins de usuario

--- src/app/controller/CheckinController.js
+++ src/app/controller/CheckinController.js
@@ -8,23 +8,30 @@ import Student from '../models/Student';
 
 class CheckinController {
     async store(req, resp) {
-        const { studentId: student_id } = req.params;
+        const schema = Yup.object().shape({
+            studentId: Yup.number(
+                'Formato de identificação do aluno está inválido.'
+            )
+                .positive('Valor de identificação do plano deve ser positivo.')
+                .integer('Formato de identificação no plano está inválido.')
+                .required('Id de Aluno obrigatório.'),
+        });
 
-        if (!student_id) {
+        if (!(await schema.isValid(req.params))) {
             return resp.status(401).json({
-                error: 'Identificação do Aluno no sistema não foi informado.',
+                error: 'Erro de validação.',
             });
         }
-
+        const { studentId: student_id } = req.params;
         const student = await Student.findByPk(student_id);
 
         if (!student) {
             return resp.status(401).json({
-                error: 'Estudante não foi encontrado.]',
+                error: 'Estudante não foi encontrado.',
             });
         }
 
-        const checkinDate = new Date().toISOString();
+        const checkinDate = new Date();
 
         const checkin = await Checkin.create({
             student: student_id,
@@ -36,6 +43,36 @@ class CheckinController {
         });
 
         return resp.json(checkin);
+    }
+
+    async index(req, resp) {
+        const schema = Yup.object().shape({
+            studentId: Yup.number(
+                'Formato de identificação do aluno está inválido.'
+            )
+                .positive('Valor de identificação do plano deve ser positivo.')
+                .integer('Formato de identificação no plano está inválido.')
+                .required('Id de Aluno obrigatório.'),
+        });
+
+        if (!(await schema.isValid(req.params))) {
+            return resp.status(401).json({
+                error: 'Erro de validação.',
+            });
+        }
+
+        const { studentId: student_id } = req.params;
+        const student = await Student.findByPk(student_id);
+
+        if (!student) {
+            return resp.status(400).json({ error: 'Aluno não encontrado.' });
+        }
+
+        const notifications = await Checkin.find({
+            student: student_id,
+        }).sort({ createdAt: 'asc' });
+
+        return resp.json(notifications);
     }
 }
 export default new CheckinController();
